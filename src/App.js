7f4bb65cfc769d4d4f493bbbb2bd08b0b2f0c3ce
Saim Ahmad <Sahmad5349@conestogac.on.ca>
2020-07-24 01:23:42 -0400

    added add and delete functions...need to hook them in application

--- src/App.js
+++ src/App.js
@@ -2,8 +2,41 @@ import React from 'react';
 import logo from './logo.jpg'
 import './App.css'
 
-
 class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      newItem:"",
+      list: []
+    }
+  }
+
+  addItem = (todoValue) =>{
+    if(todoValue != ""){
+      const newItem ={
+        id: Date.now(),
+        value: todoValue,
+        isDone: false
+      };
+      const list = [...this.state.list]
+      list.push(newItem);
+      this.setState({
+        list,
+        newItem:""
+      })
+    }
+  }
+
+  deleteItem = (id) =>{
+    const list = [...this.state.list]
+    let updatedList = list.filter(item => item.id !== id)
+    this.setState({
+      list: updatedList
+    })
+  }
+
+
   render(){
     return(
       <div>
